3766ec4e07a14faa6ff6ef5f757bd8ac5c1af701
nirozjung <zorinzung@gmail.com>
2019-11-27 21:10:43 +0100

    structuring app into Presentational & Container components

--- confusion2/src/components/MenuComponent.js
+++ confusion2/src/components/MenuComponent.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import Dishdetail from './DishdetailComponent';
 
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        };
-        console.log("Menu Component constructor called");
     }
 
     componentDidMount() {
         console.log("Menu ComponentDid Mount called");
-    }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
     }
 
     renderDish(dish) {
@@ -39,7 +30,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map(dish => {
             return (
                 <div className="col-12 col-md-5  m-1">
-                    <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+                    <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -53,7 +44,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <Dishdetail dish = {this.state.selectedDish}/>
             </div>
         );
     }
