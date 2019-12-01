f13196e136a93f19a8ceba8f24491c9229749b43
nirozjung <zorinzung@gmail.com>
2019-12-01 23:06:16 +0100

    Converting class componentto Functional Component'

--- confusion2/src/components/HeaderComponent.js
+++ confusion2/src/components/HeaderComponent.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Navbar dark>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" ref="/">
+                            <img src="assets/images/logo.png" height="30" width="41"
+                                alt="Ristorante Con Fusion" />
+                        </NavbarBrand>
+
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg">Home</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg">About Us</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg">Menu</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg">Address</span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
+
                 <Jumbotron>
-                    <div className ="container">
-                        <div className = "row row-header">
-                            <div className = "col-12 col-sm-6">
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
                                 <h1>Ristorante Con Fusion</h1>
-                                <p>We take inspiration from the World's best cusines and create unique fusion. Our lispsmacking creations will 
+                                <p>We take inspiration from the World's best cusines and create unique fusion. Our lispsmacking creations will
                                     will tickle your culinary senses.
                                 </p>
                             </div>
--- confusion2/src/components/MainComponent.js
+++ confusion2/src/components/MainComponent.js
@@ -4,6 +4,8 @@ import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import { DISHES } from '../shared/dishes';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -20,12 +22,21 @@ class Main extends Component {
     }
 
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
         return (
             <div>
-                <Header/>
-                <Menu dishes={this.state.dishes} onClick={(dishId => this.onDishSelect(dishId))} />
-                <Dishdetail dish={this.state.dishes.filter((dish) =>
-                    dish.id === this.state.selectedDish)[0]} />
+                <Header />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
